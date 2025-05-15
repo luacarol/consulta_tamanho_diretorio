e5053a02be0545d4ad5d65ef477b528264be359f
Luana Caroliny <lucarolp.deoli@gmail.com>
2025-05-15 14:57:06 -0300

    feat: creating a log file

--- consulta_tamanho_diretorio.sh
+++ consulta_tamanho_diretorio.sh
@@ -2,5 +2,5 @@
 uso_do_diretorio=$(df /Users/luana.anjos/Downloads | awk {'print $5'})
 percentual=$(echo $uso_do_diretorio | awk {'print $2'})
 if [[ $percentual =~ [7-9][0-9]%$|100% ]]; then
-        echo "ATENÇÃO - O espaço de armazenamento está ficando cheio"
+        echo "ATENÇÃO - O espaço de armazenamento está ficando cheio" >> /Users/luana.anjos/ws-luana/consulta_tamanho_diretorio/logs/compartilhamento.log
 fi
